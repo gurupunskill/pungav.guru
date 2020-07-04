32cf68f287030bb037414275337dba462f3b33a5
Gurupungav Narayanan <gurupungavn@gmail.com>
2020-07-04 15:39:26 +0530

    feat(app): #10 - Added Not found page. Closes #10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -20,6 +21,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'about',
     pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    data: { title: 'Page Not Found' }
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,20 @@ import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { BlogModule } from './blog/blog.module';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { markedOptionsFactory } from './md-renderer'
+import { markedOptionsFactory } from './md-renderer';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     PortfolioComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     MarkdownModule.forRoot({
